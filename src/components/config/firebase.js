eb15e23427e588138011202dc335bd96aa1761a7
zkilleb <zkilleb@siue.edu>
2017-11-15 15:21:59 -0600

    Add files via upload

---
+++ src/components/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyAui6CWd5xJscQRoH7bqI9-rho_-l_D2hY",
+    authDomain: "twitter-cleaner-3e810.firebaseapp.com",
+    databaseURL: "https://twitter-cleaner-3e810.firebaseio.com",
+    projectId: "twitter-cleaner-3e810",
+    storageBucket: "twitter-cleaner-3e810.appspot.com",
+    messagingSenderId: "869022372749"
+  };
+  export const app = firebase.initializeApp(config);
+  export const provider = new firebase.auth.TwitterAuthProvider();
+  export const auth = firebase.auth();
+  export const ref = firebase.database().ref();
+  export default firebase;
+  
+ 
